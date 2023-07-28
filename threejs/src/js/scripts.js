97f3bea78dcd3d142fcb8c3f26e8e4558654b21f
Cheppar <ejioforched@gmail.com>
2023-07-28 15:53:49 +0300

    add shadows

--- threejs/src/js/scripts.js
+++ threejs/src/js/scripts.js
@@ -4,6 +4,8 @@ import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 
+renderer.shadowMap.enabled = true;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
@@ -37,9 +39,11 @@ const planeMaterial = new THREE.MeshStandardMaterial(
     {   color: 0xFFFFFF,
         side: THREE.DoubleSide
     });
+
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
@@ -49,10 +53,11 @@ const sphereMaterial = new THREE.MeshStandardMaterial(
     {color: 0x00FF00,
     wireframe: false
     });
+
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
-
 sphere.position.set(-10, 10, 0);
+sphere.castShadow = true;
 
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
@@ -61,9 +66,14 @@ scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
 scene.add(directionalLight);
 directionalLight.position.set(-30, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.bottom = -12;
 
 const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
 scene.add(dLightHelper);
+
+const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper);
 
 const gui = new dat.GUI();
 
@@ -84,7 +94,6 @@ gui.add(options, 'wireframe').onChange(function(e){
 gui.add(options, 'speed', 0, 0.1);
 
 let step = 0;
-
 
 
 function animate(time) {
